7315190f4795178a71d55c22db27710c99ce84a9
Daniel <cranston.daniel@gmail.com>
2025-12-07 11:13:59 +0900

    give KinematicsPlugin virtual functions some basic body
    (interactive marker shows up when using the MotionPlanning rviz plugin)

--- crx_kinematics/src/crx_kinematics_plugin.cpp
+++ crx_kinematics/src/crx_kinematics_plugin.cpp
@@ -1,5 +1,6 @@
 #include "crx_kinematics/crx_kinematics_plugin.hpp"
 
+#include <moveit/robot_model/robot_model.hpp>
 #include <pluginlib/class_list_macros.hpp>
 namespace crx_kinematics
 {
@@ -10,7 +11,15 @@ bool CRXKinematicsPlugin::initialize(rclcpp::Node::SharedPtr const& node,
                                      std::vector<std::string> const& tip_frames,
                                      double search_discretization)
 {
-    return false;
+    storeValues(robot_model, group_name, base_frame, tip_frames, search_discretization);
+
+    const moveit::core::JointModelGroup* jmg = robot_model_->getJointModelGroup(group_name);
+
+    joint_names_ = jmg->getJointModelNames();
+    link_names_.push_back(getTipFrame());
+
+    RCLCPP_INFO(moveit::getLogger("crx_kinematics"), "tip_frame: '%s'", getTipFrame().c_str());
+    return true;
 }
 
 bool CRXKinematicsPlugin::getPositionIK(const geometry_msgs::msg::Pose& ik_pose,
@@ -19,7 +28,14 @@ bool CRXKinematicsPlugin::getPositionIK(const geometry_msgs::msg::Pose& ik_pose,
                                         moveit_msgs::msg::MoveItErrorCodes& error_code,
                                         const kinematics::KinematicsQueryOptions& options) const
 {
-    return false;
+    error_code.val = moveit_msgs::msg::MoveItErrorCodes::SUCCESS;
+    solution.clear();
+    solution.push_back(0.1);
+    for (std::size_t i = 0u; i < 5; ++i)
+    {
+        solution.push_back(0.0);
+    }
+    return true;
 }
 
 bool CRXKinematicsPlugin::searchPositionIK(const geometry_msgs::msg::Pose& ik_pose,
@@ -29,7 +45,14 @@ bool CRXKinematicsPlugin::searchPositionIK(const geometry_msgs::msg::Pose& ik_po
                                            moveit_msgs::msg::MoveItErrorCodes& error_code,
                                            const kinematics::KinematicsQueryOptions& options) const
 {
-    return false;
+    error_code.val = moveit_msgs::msg::MoveItErrorCodes::SUCCESS;
+    solution.clear();
+    solution.push_back(0.1);
+    for (std::size_t i = 0u; i < 5; ++i)
+    {
+        solution.push_back(0.0);
+    }
+    return true;
 }
 
 bool CRXKinematicsPlugin::searchPositionIK(const geometry_msgs::msg::Pose& ik_pose,
@@ -40,7 +63,14 @@ bool CRXKinematicsPlugin::searchPositionIK(const geometry_msgs::msg::Pose& ik_po
                                            moveit_msgs::msg::MoveItErrorCodes& error_code,
                                            const kinematics::KinematicsQueryOptions& options) const
 {
-    return false;
+    error_code.val = moveit_msgs::msg::MoveItErrorCodes::SUCCESS;
+    solution.clear();
+    solution.push_back(0.1);
+    for (std::size_t i = 0u; i < 5; ++i)
+    {
+        solution.push_back(0.0);
+    }
+    return true;
 }
 
 bool CRXKinematicsPlugin::searchPositionIK(const geometry_msgs::msg::Pose& ik_pose,
@@ -51,7 +81,14 @@ bool CRXKinematicsPlugin::searchPositionIK(const geometry_msgs::msg::Pose& ik_po
                                            moveit_msgs::msg::MoveItErrorCodes& error_code,
                                            const kinematics::KinematicsQueryOptions& options) const
 {
-    return false;
+    error_code.val = moveit_msgs::msg::MoveItErrorCodes::SUCCESS;
+    solution.clear();
+    solution.push_back(0.1);
+    for (std::size_t i = 0u; i < 5; ++i)
+    {
+        solution.push_back(0.0);
+    }
+    return true;
 }
 
 bool CRXKinematicsPlugin::searchPositionIK(const geometry_msgs::msg::Pose& ik_pose,
@@ -63,14 +100,42 @@ bool CRXKinematicsPlugin::searchPositionIK(const geometry_msgs::msg::Pose& ik_po
                                            moveit_msgs::msg::MoveItErrorCodes& error_code,
                                            const kinematics::KinematicsQueryOptions& options) const
 {
-    return false;
+    error_code.val = moveit_msgs::msg::MoveItErrorCodes::SUCCESS;
+    solution.clear();
+    solution.push_back(0.1);
+    for (std::size_t i = 0u; i < 5; ++i)
+    {
+        solution.push_back(0.0);
+    }
+    return true;
 }
 
 bool CRXKinematicsPlugin::getPositionFK(const std::vector<std::string>& link_names,
                                         const std::vector<double>& joint_angles,
                                         std::vector<geometry_msgs::msg::Pose>& poses) const
 {
-    return false;
+    poses.clear();
+    for (std::size_t i = 0u; i < link_names.size(); ++i)
+    {
+        poses.push_back(geometry_msgs::msg::Pose());
+    }
+    return true;
+}
+
+bool CRXKinematicsPlugin::getPositionIK(const std::vector<geometry_msgs::msg::Pose>& ik_poses,
+                                        const std::vector<double>& ik_seed_state,
+                                        std::vector<std::vector<double> >& solutions,
+                                        kinematics::KinematicsResult& result,
+                                        const kinematics::KinematicsQueryOptions& options) const
+{
+    solutions.clear();
+    for (std::size_t i = 0u; i < ik_poses.size(); ++i)
+    {
+        solutions.push_back({ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 });
+    }
+    result.kinematic_error = kinematics::KinematicError::OK;
+    result.solution_percentage = 1.0;
+    return true;
 }
 
 std::vector<std::string> const& CRXKinematicsPlugin::getJointNames() const
